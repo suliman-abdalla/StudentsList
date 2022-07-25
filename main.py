f764a2826d54a7d4e4c85427c45c639a37cd6a5f
Suliman Abdalla <suliman.zein2@gmail.com>
2022-07-25 16:08:32 +0400

    Saving to File is working fine

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import ast
 students = {}
 class Db:
 
     def add(stud):
-        students=stud
+        students = stud
+        try:
+            f = open('Data', 'r')
+            sh = f.readline()
+            sh1 = ast.literal_eval(sh)
+            students=sh1
+        except Exception:
+            pass
         student=[]
         x=input('\nStudent ID: ')
         y=input('Student Name: ')
@@ -14,71 +22,109 @@ class Db:
         elif z <=49:
             student.append("Fail")
         students[x]=student
+        strr = str(students)
+        f = open('Data', 'w')
+        f.write(strr)
         return students
 
     def modi(stud):
-        students=stud
-        student=[]
-        a=input("Enter Student ID to Edit: ")
-        y=str(input('New Student Name: '))
-        z=int(input('New Student Mark: '))
-        student.append(y)
-        student.append(z)
-        if z >=50:
-            student.append("Pass")
-        elif z <=49:
-            student.append("Fail")
-        students[a]=student
-        return students
+        try:
+            f = open('Data', 'r')
+            sh = f.readline()
+            sh1 = ast.literal_eval(sh)
+            students = sh1
+            student=[]
+            a=input("Enter Student ID to Edit: ")
+            y=str(input('New Student Name: '))
+            z=int(input('New Student Mark: '))
+            student.append(y)
+            student.append(z)
+            if z >=50:
+                student.append("Pass")
+            elif z <=49:
+                student.append("Fail")
+            students[a]=student
+            strr = str(students)
+            f = open('Data', 'w')
+            f.write(strr)
+            return students
+        except Exception:
+            print('No student Data Present...')
 
     def del1(data):
+        try:
+            f = open('Data', 'r')
+            sh = f.readline()
+            sh1 = ast.literal_eval(sh)
+            students = sh1
 
-        id=input("Enter Student ID to delete: ")
-        data.pop(id,"ID Doesn't Exist")
-        return data
+            id=input("Enter Student ID to delete: ")
+            sh1.pop(id,"ID Doesn't Exist")
+            strr = str(students)
+            f = open('Data', 'w')
+            f.write(strr)
+            return sh1
+        except Exception:
+            print('No student Data Present...')
 
     def dele(dd):
-
         print('This action will delete all students Data!!!')
         d=str(input('Proceed? Y/N: '))
         if d == 'Y':
             dd = {}
             globals()['students'] = {}
+            f = open('Data', 'w')
+            f.write("")
+            print('All students data is deleted...')
+            return dd
+        elif d == 'y':
+            dd = {}
+            globals()['students'] = {}
+            f = open('Data', 'w')
+            f.write("")
+            print('All students data is deleted...')
             return dd
         else:
             print('List is Not deleted')
 
     def show(students):
-
-        print()
-        print("{:<8} {:<15} {:<10} {:<10}".format('ID', 'NAME', 'MARK', 'PASS?'))
-
-        for key, value in students.items():
-            name1, mark1, pa = value
-            print("{:<8} {:<15} {:<10} {:<10}".format(key, name1, mark1, pa))
+        f = open('Data', 'r')
+        sh = f.readline()
+        try:
+            sh1 = ast.literal_eval(sh)
+            print()
+            print("{:<8} {:<15} {:<10} {:<10}".format('ID', 'NAME', 'MARK', 'PASS?'))
+            for key, value in sh1.items():
+                name1, mark1, pa = value
+                print("{:<8} {:<15} {:<10} {:<10}".format(key, name1, mark1, pa))
+        except Exception:
+            print("No student Data Present...")
 
 class Main:
     com1=Db()
     list={}
     choice=1
+    try:
+        file=open('Data','x')
+    except Exception:
+        pass
     print('** Welcome to Students Database **\n')
-    while choice != 6:
+    while choice != '6':
         print('\n---------------------------- \nPlease Select Your Action: '
               '\n 1: Add New Student\n 2: Show Current Students\n 3: Edit a Student\n 4: Delete a Student\n 5: Delete All Students\n 6: Exit\n')
-        choice=int(input(' Selection = '))
-        if choice==1:
+        choice=str(input(' Selection = '))
+        if choice=='1':
             list=Db.add(list)
-        elif choice==2:
+        elif choice=='2':
             Db.show(list)
-        elif choice==3:
+        elif choice=='3':
             list=Db.modi(list)
-        elif choice==4:
+        elif choice=='4':
             list=Db.del1(list)
-        elif choice == 5:
-            print(list)
+        elif choice == '5':
             delist = list
             list=Db.dele(delist)
-        elif choice == 6:
+        elif choice == '6':
             print("Good Bye")
         else:
             print('\ninvalid Selection')
